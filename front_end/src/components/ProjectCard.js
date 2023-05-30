31bcdf7410e4947e8f50345a67d77c2ecfa89fe3
jayserver <jhjo197@naver.com>
2023-05-30 12:40:38 +0900

    project1 renewal and modified images

--- front_end/src/components/ProjectCard.js
+++ front_end/src/components/ProjectCard.js
@@ -1,7 +1,6 @@
 import { Col, Modal, Button} from "react-bootstrap";
 import { useState } from "react";
 import './css/modal-custom.css';
-import ReactPlayer from 'react-player/youtube';
 
 export const ProjectCard_temp = ({ title, description, imgUrl }) => {
 
@@ -22,37 +21,7 @@ export const ProjectCard = ({
   title, 
   thumbnail, 
   thumbnail_description, 
-  videoUrl1, video1_description, 
-  imgUrl1, 
-  imgUrl2, 
-  imgUrl3,
-  imgUrl3_description, 
-  imgUrl3_1, 
-  imgUrl3_2, 
-  imgUrl3_2_description, 
-  imgUrl3_3, 
-  imgUrl3_3_description,
-  imgUrl4,
-  imgUrl4_1, 
-  imgUrl4_description, 
-  imgUrl10, 
-  description1_title, 
-  description1_sub_title, 
-  description1, description2_title, 
-  description2_sub_title, 
-  description2, 
-  description3_title, 
-  description3_sub_title, 
-  description3_sub_title1, 
-  description3_sub_title2,
-  description3, description3_1, 
-  description3_2, 
-  description4_title, 
-  description4_sub_title, 
-  description4, 
-  description10_title, 
-  description10_sub_title, 
-  description10 
+  main_body,
 }) => {
   const [showModal, setShowModal] = useState(false);
 
@@ -74,83 +43,11 @@ export const ProjectCard = ({
         </div>
       </div>
         <Modal show={showModal} onHide={handleModalClose}>
-          <Modal.Header tiitle>
+          <Modal.Header title>
               <Modal.Title><h2>{title}</h2></Modal.Title> 
           </Modal.Header>
             <Modal.Body>
-              {description1_title && <h3>{description1_title}</h3>}
-              {description1_sub_title && <h4>{description1_sub_title}</h4>}
-              {imgUrl1 && <img src={imgUrl1} alt={title} />}
-              {description1 && <span>{description1}</span>} 
-
-              {description2_title && <h3>{description2_title}</h3>}
-              {description2_sub_title && <h3>{description2_sub_title}</h3>}
-              {/* {videoUrl1 && (
-                <video className="project-video" controls autoPlay onLoadedMetadata={(e) => {e.target.playbackRate = 1.5; }}>
-                  <source src={videoUrl1} type="video/mp4" />
-                  Your browser does not support the video tag.
-                </video>
-              )} */}
-              {videoUrl1 && (
-              <ReactPlayer
-                className="project-video"
-                url='https://youtu.be/ZFGxTAT8I4o'
-                controls
-                playing
-                playbackRate={1.25}
-              />
-              )}
-              {video1_description && <span_sub fontSize='18px'>{video1_description}</span_sub>}
-              {imgUrl2 && <img src={imgUrl2} alt={title} />}
-              {description2 && <span>{description2}</span>}
-
-              {description3_title && <h3>{description3_title}</h3>}
-                {description3_sub_title && <h4>{description3_sub_title}</h4>}
-                {imgUrl3 && (
-                  <div style={{ display: 'flex', justifyContent: 'center' }}>
-                    <img src={imgUrl3} alt={title} style={{ width: '95%', border: '1px solid rgba(0, 0, 0, 0.5)' }} />
-                  </div>
-                )}
-                {imgUrl3_description && <span_sub>{imgUrl3_description}</span_sub>}
-                {description3 && <span>{description3}</span>}
-                {imgUrl3_1 && (
-                  <div style={{ display: 'flex', justifyContent: 'center' }}>
-                    <img src={imgUrl3_1} alt={title} style={{ width: '100%', border: '1px solid rgba(0, 0, 0, 0.5)' }} />
-                  </div>
-                )}
-                {description3_sub_title1 && <h4>{description3_sub_title1}</h4>}
-                {imgUrl3_2 && (
-                  <div style={{ display: 'flex', justifyContent: 'center' }}>
-                    <img src={imgUrl3_2} alt={title} style={{ width: '65%', border: '1px solid rgba(0, 0, 0, 0.5)' }} />
-                  </div>
-                )}
-                {imgUrl3_2_description && <span_sub>{imgUrl3_2_description}</span_sub>}
-                {description3_1 && <span>{description3_1}</span>}
-                {description3_sub_title2 && <h4>{description3_sub_title2}</h4>}
-                {imgUrl3_3 && (
-                  <div style={{ display: 'flex', justifyContent: 'center' }}>
-                    <img src={imgUrl3_3} alt={title} style={{ width: '90%', border: '1px solid rgba(0, 0, 0, 0.5)' }} />
-                  </div>
-                )}
-                {imgUrl3_3_description && <span_sub>{imgUrl3_3_description}</span_sub>}
-                {description3_2 && <span>{description3_2}</span>}
-
-                {description4_title && <h3>{description4_title}</h3>}
-                {description4_sub_title && <h4>{description4_sub_title}</h4>}
-                {imgUrl4 && (
-                  <div style={{ display: 'flex', justifyContent: 'center' }}>
-                    <img src={imgUrl4} alt={title} style={{ width: '95%', border: '1px solid rgba(0, 0, 0, 0.5)' }} />
-                  </div>
-                )}
-                {imgUrl4_description && <span_sub>{imgUrl4_description}</span_sub>}
-                {description4 && <span>{description4}</span>}
-
-
-              {description10_title && <h3>{description10_title}</h3>}
-              {description10_sub_title && <h4>{description10_sub_title}</h4>}
-              {imgUrl10 && <img src={imgUrl10} alt={title} />}
-              {description10 && <span>{description10}</span>}
-
+              {main_body && <span>{main_body}</span>}
               {/* Add more project information as needed */}
             </Modal.Body>
           <Modal.Footer>
